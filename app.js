a9388f1ca8148b86a4bfc9737890b9803dbb2dbc
MuhammadHammad002 <hammadfayyazpolani@gmail.com>
2024-02-26 13:57:57 +0500

    finished

---
+++ app.js
@@ -0,0 +1,65 @@
+//Chap1
+//alert("Hello")
+//Q1
+//alert("Error! Please enter a valid password")
+//Q2
+//alert("Welcome to JS Land...\n Happy Coding!")
+//Q3
+//alert("Welcome to JS Land...")
+//alert("Happy Coding!")
+//Q4
+//alert("Hello... I can run JS through my web browser's console")
+//console.log("Hello... I can run JS through my web browser's")
+//Q5
+
+//Chap2
+// var userName = "Hammad Fayyaz" 
+// alert(userName)
+//Q1
+// var myName = "Hammad Fayyaz Polani"
+// alert(myName)
+//Q2
+// var message = "Hello World"
+//  alert(message)
+//Q3
+// var fullName ="Hammad Fayyaz"
+// var age ="19 years old"
+// var degree ="Certified Mobile Application Developement"
+// alert(fullName)
+// alert(age)
+// alert(degree)
+//Q4
+// var item1 = "Pizza\nPizz\nPiz\nPi\nP\n"
+// alert(item1)
+// Q5
+// var msg = "My email address is"
+// var email ="hammadfayyazpolani@gmail.com"
+// alert(msg+" "+email)
+//Q6
+// var msg2 = "I am trying to learn from the Book"
+// var book ="A smarter\n way to learn JavaScript"
+// alert(msg2+" "+book)
+//Q7
+//document.write("Yah! I can write HTML content through JavaScript")
+//Q8
+// var icon = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬"
+// alert(icon)
+//Q9
+
+//Chap3
+// var age =" I am 19 years old"
+// alert(age)
+//Q1
+// var visited = "You have visited this site N times"
+// alert(visited)
+// var birthYear = "My birth year is 2004 \n Data type of my ddeclared variable is number"
+// document.write(birthYear)
+//Q3
+// var msg3 = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store"
+// document.write(msg3)
+
+//Q4
+
+
+
+
